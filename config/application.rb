0ef1cbbfdad0e46aba781b0a2de52ab073a56cc7
degasm <marcel.degas@autodesk.com>
2020-01-25 15:33:33 -0800

    adds boostrap gem and config

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module LibraryApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
